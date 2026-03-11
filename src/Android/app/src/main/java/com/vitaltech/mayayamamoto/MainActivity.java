bae4b14926930e1515055aba672846d2849de953
Murilo Ângelo <murapb07@gmail.com>
2026-03-11 15:37:00 -0300

    Tela principal criada com carrossel e ajustes no cadastro

--- src/Android/app/src/main/java/com/vitaltech/mayayamamoto/MainActivity.java
+++ src/Android/app/src/main/java/com/vitaltech/mayayamamoto/MainActivity.java
@@ -1,24 +1,125 @@
 package com.vitaltech.mayayamamoto;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
+
+import com.google.android.material.carousel.CarouselLayoutManager;
+
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView carouselRecyclerView;
+    private CarouselAdapter adapter;
+    private int currentIndex = 0;
+    private final Handler autoScrollHandler = new Handler();
+    private final int AUTO_SCROLL_INTERVAL = 3000; // milisegundos
+
+    Button btStart;
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btStart = findViewById(R.id.btStart);
+
+        btStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Carrossel
+        carouselRecyclerView = findViewById(R.id.recyclerView);
+
+        // Setup Carousel Layout Manager
+        CarouselLayoutManager carouselLayoutManager = new CarouselLayoutManager();
+        carouselRecyclerView.setLayoutManager(carouselLayoutManager);
+
+        List<Integer> images = Arrays.asList(
+                R.drawable.psicossomatica,
+                R.drawable.kabat,
+                R.drawable.massagem
+        );
+
+        adapter = new CarouselAdapter(images);
+        carouselRecyclerView.setAdapter(adapter);
+
+        SnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(carouselRecyclerView);
+
+        carouselRecyclerView.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    stopAutoScroll();
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    startAutoScroll();
+                    break;
+            }
+            return false;
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    private void startAutoScroll(){
+        autoScrollHandler.postDelayed(autoScrollRunnable, AUTO_SCROLL_INTERVAL);
+    }
+
+    private void stopAutoScroll(){
+        autoScrollHandler.removeCallbacks(autoScrollRunnable);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        startAutoScroll();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopAutoScroll();
+    }
+
+    private final Runnable autoScrollRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (adapter.getItemCount() == 0) return;
+
+            currentIndex = (currentIndex + 1) % adapter.getItemCount();
+            carouselRecyclerView.smoothScrollToPosition(currentIndex);
+
+            autoScrollHandler.postDelayed(this, AUTO_SCROLL_INTERVAL);
+        }
+    };
 }
